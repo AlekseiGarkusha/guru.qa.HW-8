6550b2a464b0b87049d4d3c1e4dee09d1ab9cd6f
AlekseiGarkusha <garkusha.ag@mail.ru>
2026-03-27 11:58:22 +0300

    refactoring

--- src/test/java/tests/hw/HwParsingFiles.java
+++ src/test/java/tests/hw/HwParsingFiles.java
@@ -3,7 +3,9 @@ package tests.hw;
 import com.codeborne.pdftest.PDF;
 
 import com.codeborne.xlstest.XLS;
+import com.opencsv.CSVParserBuilder;
 import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import tests.FileParsingTest;
@@ -36,7 +38,6 @@ public class HwParsingFiles {
   @Test
   void parsingPdfFromZip() throws Exception {
     byte[] content = readEntry("Price-Crimea-Electric-2025.pdf");
-    System.out.println("Тут отображается название файла: " + content);
     PDF pdf = new PDF(new ByteArrayInputStream(content));
     Assertions.assertTrue(pdf.text.contains("Электромонтаж в Крыму"));
   }
@@ -45,40 +46,41 @@ public class HwParsingFiles {
   void parsingCsvFromZip() throws Exception {
     byte[] content = readEntry("username.csv");
 
-    System.out.println("Тут отображается название файла: " + content);
-    try (CSVReader csvReader = new CSVReader(
+    CSVReader csvReader = new CSVReaderBuilder(
       new InputStreamReader(
         new ByteArrayInputStream(content),
-        StandardCharsets.UTF_8))) {
+        StandardCharsets.UTF_8))
+      .withCSVParser(new CSVParserBuilder()
+        .withSeparator(';')
+        .build())
+      .build();
 
-      List<String[]> data = csvReader.readAll();
+    List<String[]> data = csvReader.readAll();
 
-      for (String[] row : data) {
-        for (int i = 0; i < row.length; i++) {
-          row[i] = row[i].trim();
-        }
+    for (String[] row : data) {
+      for (int i = 0; i < row.length; i++) {
+        row[i] = row[i].trim();
       }
-
-      Assertions.assertArrayEquals(new String[]{"Username", "Identifier", "First name", "Last name"}, data.get(0));
-      Assertions.assertArrayEquals(new String[]{"booker12", "9012", "Rachel", "Booker"}, data.get(1));
-      Assertions.assertArrayEquals(new String[]{"grey07", "2070", "Laura", "Grey"}, data.get(2));
-      Assertions.assertArrayEquals(new String[]{"johnson81", "4081", "Craig", "Johnson"}, data.get(3));
-      Assertions.assertArrayEquals(new String[]{"jenkins46", "9346", "Mary", "Jenkins"}, data.get(4));
-      Assertions.assertArrayEquals(new String[]{"smith79", "5079", "Jamie", "Smith"}, data.get(5));
     }
+
+    Assertions.assertArrayEquals(new String[]{"Username", "Identifier", "First name", "Last name"}, data.get(0));
+    Assertions.assertArrayEquals(new String[]{"booker12", "9012", "Rachel", "Booker"}, data.get(1));
+    Assertions.assertArrayEquals(new String[]{"grey07", "2070", "Laura", "Grey"}, data.get(2));
+    Assertions.assertArrayEquals(new String[]{"johnson81", "4081", "Craig", "Johnson"}, data.get(3));
+    Assertions.assertArrayEquals(new String[]{"jenkins46", "9346", "Mary", "Jenkins"}, data.get(4));
+    Assertions.assertArrayEquals(new String[]{"smith79", "5079", "Jamie", "Smith"}, data.get(5));
+  }
+
+
+  @Test
+  void parsingXlsFromZip() throws Exception {
+    byte[] content = readEntry("testXlsx.xlsx");
+    XLS xls = new XLS(new ByteArrayInputStream(content));
+
+    String actualValue = String.valueOf(xls.excel.getSheetAt(0).getRow(0).getCell(0));
+    Assertions.assertTrue(actualValue.contains("TestData"));
   }
 }
-
-//  @Test
-//  void parsingXlsFromZip() throws Exception {
-//    byte[] content = readEntry("testXlsx.xlsx");
-//    System.out.println("Тут отображается название файла: " + content);
-//    XLS xls = new XLS(new ByteArrayInputStream(content));
-//
-//    String actualValue = String.valueOf(xls.excel.getSheetAt(0).getRow(0).getCell(0));
-//    Assertions.assertTrue(actualValue.contains("TestData"));
-//  }
-
 
 
 
