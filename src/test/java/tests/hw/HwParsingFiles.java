8dd679cdd56bdd41281c7a66aa51bbf399bb1ceb
Alex <sapphire6061@gmail.com>
2026-03-27 07:04:50 +0300

    WIP

--- src/test/java/tests/hw/HwParsingFiles.java
+++ src/test/java/tests/hw/HwParsingFiles.java
@@ -3,14 +3,13 @@ package tests.hw;
 import com.codeborne.pdftest.PDF;
 
 import com.codeborne.xlstest.XLS;
-import com.opencsv.CSVParserBuilder;
 import com.opencsv.CSVReader;
-import com.opencsv.CSVReaderBuilder;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import tests.FileParsingTest;
 
 import java.io.*;
+
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.zip.ZipEntry;
@@ -19,102 +18,64 @@ import java.util.zip.ZipInputStream;
 public class HwParsingFiles {
   private ClassLoader cl = FileParsingTest.class.getClassLoader();
 
-  private byte[] readEntry(ZipInputStream zip) throws IOException {
-    ByteArrayOutputStream baos = new ByteArrayOutputStream();
-    byte[] buffer = new byte[1024];
-    int len;
-
-    while ((len = zip.read(buffer)) != -1) {
-      baos.write(buffer, 0, len);
+  private byte[] readEntry(String zipFileName) throws Exception {
+    try (ZipInputStream zip = new ZipInputStream(cl.getResourceAsStream("TestFIles.zip"))) {
+      ZipEntry entry;
+      while ((entry = zip.getNextEntry()) != null) {
+        System.out.println("ENTRY: [" + entry.getName() + "] LENGTH: " + entry.getSize());
+        System.out.print("BYTES: ");
+        for (byte b : entry.getName().getBytes(StandardCharsets.UTF_8)) System.out.print(b + " ");
+        System.out.println();
+      }
     }
-
-    return baos.toByteArray();
+    return new byte[0];
   }
 
   @Test
-  void zipHwTest() throws Exception {
-    try (ZipInputStream zip = new ZipInputStream(
-      cl.getResourceAsStream("testHW.zip")
-    )) {
-      ZipEntry entry;
+  void parsingPdfFromZip() throws Exception {
+    byte[] content = readEntry("Price-Crimea-Electric-2025.pdf");
+    System.out.println("Тут отображается название файла: " + content);
+    PDF pdf = new PDF(new ByteArrayInputStream(content));
+    Assertions.assertTrue(pdf.text.contains("Электромонтаж в Крыму"));
+  }
 
-      // проходимся по архиву, до конца, записываем все названия
-      while ((entry = zip.getNextEntry()) != null) {
-        String entryName = entry.getName();
-        byte[] content = readEntry(zip);
+  @Test
+  void parsingCsvFromZip() throws Exception {
+    byte[] content = readEntry("username.csv");
 
-        //PDF
-        // Если название оканчивается на .pdf - то создадим файл
-        if (entryName.endsWith(".pdf")) {
+    System.out.println("Тут отображается название файла: " + content);
+    try (CSVReader csvReader = new CSVReader(
+      new InputStreamReader(
+        new ByteArrayInputStream(content),
+        StandardCharsets.UTF_8))) {
 
-          File testPdf = File.createTempFile("temp", ".pdf");
+      List<String[]> data = csvReader.readAll();
 
-          try (FileOutputStream fos = new FileOutputStream(testPdf)) {
-            fos.write(content);
-          }
-
-          PDF pdf = new PDF(testPdf);
-          Assertions.assertTrue(
-            pdf.text.contains("Электромонтаж в Крыму"),
-            "Заголовок в pdf документе не найден"
-          );
-        }
-
-        // CSV
-        // Если название оканчивается на .csv - то создадим файл
-
-        if (entryName.endsWith(".csv")) {
-          try (CSVReader csvReader = new CSVReaderBuilder(
-            new InputStreamReader(
-              new ByteArrayInputStream(content),
-              StandardCharsets.UTF_8))
-            .withCSVParser(new CSVParserBuilder().withSeparator(';').build())
-            .build()) {
-
-            List<String[]> data = csvReader.readAll();
-
-            for (String[] row : data) {
-              for (int i = 0; i < row.length; i++) {
-                row[i] = row[i].trim();
-              }
-            }
-
-
-            Assertions.assertArrayEquals(new String[]{"Username", "Identifier", "First name", "Last name"}, data.get(0));
-            Assertions.assertArrayEquals(new String[]{"booker12", "9012", "Rachel", "Booker"}, data.get(1));
-            Assertions.assertArrayEquals(new String[]{"grey07", "2070", "Laura", "Grey"}, data.get(2));
-            Assertions.assertArrayEquals(new String[]{"johnson81", "4081", "Craig", "Johnson"}, data.get(3));
-            Assertions.assertArrayEquals(new String[]{"jenkins46", "9346", "Mary", "Jenkins"}, data.get(4));
-            Assertions.assertArrayEquals(new String[]{"smith79", "5079", "Jamie", "Smith"}, data.get(5));
-          }
-        }
-
-        // xls
-        if (entry.getName().endsWith(".xlsx")) {
-
-          File testXlsx = File.createTempFile("temp", ".xlsx");
-
-          try (FileOutputStream fos = new FileOutputStream(testXlsx)) {
-            fos.write(content);
-          }
-
-          XLS xls = new XLS(testXlsx);
-          System.out.println(testXlsx);
-
-          String actualValue = String.valueOf(xls.excel.getSheetAt(0).getRow(0).getCell(0));
-
-          System.out.println("То что ищем в excel:" + actualValue);
-          Assertions.assertTrue(actualValue.contains("TestData"));
+      for (String[] row : data) {
+        for (int i = 0; i < row.length; i++) {
+          row[i] = row[i].trim();
         }
       }
+
+      Assertions.assertArrayEquals(new String[]{"Username", "Identifier", "First name", "Last name"}, data.get(0));
+      Assertions.assertArrayEquals(new String[]{"booker12", "9012", "Rachel", "Booker"}, data.get(1));
+      Assertions.assertArrayEquals(new String[]{"grey07", "2070", "Laura", "Grey"}, data.get(2));
+      Assertions.assertArrayEquals(new String[]{"johnson81", "4081", "Craig", "Johnson"}, data.get(3));
+      Assertions.assertArrayEquals(new String[]{"jenkins46", "9346", "Mary", "Jenkins"}, data.get(4));
+      Assertions.assertArrayEquals(new String[]{"smith79", "5079", "Jamie", "Smith"}, data.get(5));
     }
   }
 }
 
-
-
-
-
+//  @Test
+//  void parsingXlsFromZip() throws Exception {
+//    byte[] content = readEntry("testXlsx.xlsx");
+//    System.out.println("Тут отображается название файла: " + content);
+//    XLS xls = new XLS(new ByteArrayInputStream(content));
+//
+//    String actualValue = String.valueOf(xls.excel.getSheetAt(0).getRow(0).getCell(0));
+//    Assertions.assertTrue(actualValue.contains("TestData"));
+//  }
 
 
 
